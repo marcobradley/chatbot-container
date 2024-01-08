74ffb0f33230957e86ec786930e64e61dc8068ac
marcobradley <marco.bradley@gmail.com>
2024-01-07 20:05:16 -0500

    updating style sheet to show properly

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 const api_key = process.env.OPENAI_API_KEY 
-
+app.use(express.static(__dirname + '/css'));
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
